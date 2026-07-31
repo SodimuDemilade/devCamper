0c02abae1004ef5bfe9f18e9ce389a9dbcd17cee
SodimuDemilade <demisodimu@gmail.com>
2026-07-31 18:11:22 -0400

    Populated get single bootcamp with courses

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -18,7 +18,7 @@ export const getBootcamps = asyncHandler(async (req, res, next) => {
 // @route  -   GET /api/v1/bootcamps/:id
 // @access -   Public
 export const getBootcamp = asyncHandler(async (req, res, next) => {
-    const bootcamp = await Bootcamp.findById(req.params.id);
+    const bootcamp = await Bootcamp.findById(req.params.id).populate("courses");
 
     if (!bootcamp) {
         return next(
