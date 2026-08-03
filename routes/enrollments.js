eea386080a675a99ca31d4f12ac21477b404b924
SodimuDemilade <demisodimu@gmail.com>
2026-08-02 20:20:48 -0400

    Removed authorization from get enrollments by bootcamps

--- routes/enrollments.js
+++ routes/enrollments.js
@@ -24,7 +24,7 @@ router.route("/").get(
 
 router.route("/user").get(protect, getEnrollmentByUser);
 
-router.route("/bootcamp/:bootcampId").get(protect, authorize("publisher", "admin"), getEnrollmentByBootcamp);
+router.route("/bootcamp/:bootcampId").get(protect, getEnrollmentByBootcamp);
 
 router.route("/enroll").post(protect, authorize("user"), addEnrollment);
 
